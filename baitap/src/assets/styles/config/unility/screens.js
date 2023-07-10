b34c51c458d130b1b03cc883cfdb60d9a2148176
thinhvu2801 <minhthinhhero@gmail.com>
2023-07-10 14:20:27 +0700

    10-7-2023 demo

--- baitap/src/assets/styles/config/unility/screens.js
+++ baitap/src/assets/styles/config/unility/screens.js
@@ -4,7 +4,7 @@ const Screens = {
   'md': '768px',
   'lg': '992px',
   'xl': '1280px',
-  '2xl': '1920px',
+  '2xl': '1800px',
   '2k': '2000px',
   'down_xl': {'max': '1199px'},
   'down_lg': {'max': '991px'},
